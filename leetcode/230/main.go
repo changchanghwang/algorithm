fd41a7bc7f7c0b550c654d3c92c423f94db115a0
changchanghwang <hch950627@naver.com>
2024-08-17 13:46:58 +0900

    leetcode: 230. Kth Smallest Element in a BST

---
+++ leetcode/230/main.go
@@ -0,0 +1,31 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+
+}
+
+func walk(root *TreeNode, c chan int) {
+	if root == nil {
+		return
+	}
+
+	walk(root.Left, c)
+	c <- root.Val
+	walk(root.Right, c)
+}
+
+func kthSmallest(root *TreeNode, k int) int {
+	c := make(chan int)
+	go walk(root, c)
+
+	for i := 0; i < k-1; i++ {
+		<-c
+	}
+	return <-c
+}
