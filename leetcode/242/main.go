99e99037f89153efc872a952da68a5783560f224
changchanghwang <hch950627@naver.com>
2024-08-22 17:22:06 +0900

    leetcode: 242. Valid Anagram
    https://leetcode.com/problems/valid-anagram/description/

---
+++ leetcode/242/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "sort"
+
+func main() {
+	s := "anagram"
+	t := "nagaram"
+	result := isAnagram(s, t)
+	println(result)
+	result = isAnagram2(s, t)
+	println(result)
+}
+
+func isAnagram(s string, t string) bool {
+	runes1 := []rune(s)
+	runes2 := []rune(t)
+	sort.Slice(runes1, func(i, j int) bool {
+		return runes1[i] < runes1[j]
+	})
+	sort.Slice(runes2, func(i, j int) bool {
+		return runes2[i] < runes2[j]
+	})
+
+	return string(runes1) == string(runes2)
+}
+
+func isAnagram2(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	count := make([]int, 26)
+
+	for index, _ := range count {
+		count[index] = 0
+	}
+
+	for i := 0; i < len(s); i++ {
+		count[int(s[i])-int('a')]++
+		count[int(t[i])-int('a')]--
+	}
+
+	for _, val := range count {
+		if val != 0 {
+			return false
+		}
+	}
+	return true
+}
