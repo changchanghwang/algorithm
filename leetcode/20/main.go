5ce2cd7b06f92e6e296b96f2bf9c587de62ee69a
changchanghwang <hch950627@naver.com>
2024-11-27 21:20:21 +0900

    leetcode: 20. Valid Parentheses

---
+++ leetcode/20/main.go
@@ -0,0 +1,39 @@
+package main
+
+func main () {
+
+	s := "()}"
+	println(isValid(s))
+
+}
+
+func isValid(s string) bool {
+	stack := []rune{}
+
+	for _, c := range s {
+		if c == '(' || c =='{' || c == '[' {
+			stack = append(stack, c)
+		}		
+
+		if c == ')' {
+			if len(stack) == 0 || stack[len(stack)-1] != '(' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+		if c == '}' {
+			if len(stack) == 0 || stack[len(stack)-1] != '{' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+		if c == ']' {
+			if len(stack) == 0 || stack[len(stack)-1] != '[' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return len(stack) ==0
+}
